956fc1acca5553c7d399944bbf9700ad7a9c172e
Wesley Silva <wesleysa95@gmail.com>
2020-08-06 22:22:56 -0300

    Estilizando o page header

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
+import PageHeader from '../../components/PageHeader';
+import styles from './styles';
 
 function Favorites () {
   return (
-    <View>
-      
+    <View style={styles.container}>
+      <PageHeader title="Meus Proffys favoritos"/>  
     </View>
   )
 }
